062ef814ba1593d4de32a349629bd0bda16a66a0
Miles Poland <miles.h.poland@gmail.com>
2024-11-24 17:04:14 -0600

    The process continues

--- Bank.java
+++ Bank.java
@@ -11,17 +11,33 @@ public class Bank{
             accountBalance = f_accountBalance;
             name = f_name;
         }
-        public int modifyBalance(double amount){ // Just put in a negative number to withdraw
+        private int modifyBalance(double amount){ // Just put in a negative number to withdraw
             if((accountBalance + amount) < 0){
                 return 1; //Insufficient funds
             }
             accountBalance += amount;
             return 0; //Success
         }
+        public String getName(){
+            return name;
+        }
+        public int setName(String f_newName){
+            name = f_newName;
+            return 0;
+        }
     }
     ArrayList<Account> accountList = new ArrayList<Account>();
     String bankName;
+    int curId;
     public Bank(String f_bankName){
 
+    }
+    public int addAccount(String f_name){
+        accountList.add(new Account(curId, 0.0, f_name));
+        curId++;
+        return 0;
+    }
+    public int withdraw(double f_amount, int f_accountId){
+        return accountList.get(f_accountId).modifyBalance(-f_amount);
     }
 }
