c4be3cc15745cf502633914078991436b9ce2192
Miles Poland <miles.h.poland@gmail.com>
2024-11-24 16:47:14 -0600

    Refactored to be in an all-in-one file

--- Bank.java
+++ Bank.java
@@ -1,1 +1,15 @@
+public class Bank{
+    private class Account{
+        private int id;
+        private double accountBalance;
+        private String name;
 
+        public Account(int f_id, double f_accountBalance, String f_name){
+            id = f_id;
+            accountBalance = f_accountBalance;
+            name = f_name;
+        }
+        
+        
+    }
+}
